76976267b67725ba6573ebb7d0840912b89a7bc2
JoãoGabriel <jogabriel433@gmail.com>
2025-04-22 16:02:44 -0300

    deploy não corrigido

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig = {
-    assetPrefix: '/tarefa-4-spotify-trends', // Certifique-se de que está correto
-    basePath: '/tarefa-4-spotify-trends',
-    output: 'export',
-    reactStrictMode: true,
+  assetPrefix: isGithubPages ? '/tarefa-4-spotify-trends' : '',
+  basePath: isGithubPages ? '/tarefa-4-spotify-trends' : '',
+  output: 'export',
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig;
