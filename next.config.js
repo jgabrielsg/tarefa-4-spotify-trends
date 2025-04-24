dfff7a7a1021fb773288ba565492ccd19f4575eb
JoãoGabriel <jogabriel433@gmail.com>
2025-04-24 10:24:30 -0300

    deploy dnv

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
 const nextConfig = {
-  assetPrefix: isGithubPages ? '/tarefa-4-spotify-trends' : '',
-  basePath: isGithubPages ? '/tarefa-4-spotify-trends' : '',
+  assetPrefix: '/tarefa-4-spotify-trends',
+  basePath: '/tarefa-4-spotify-trends',
   output: 'export',
   reactStrictMode: true,
 }
