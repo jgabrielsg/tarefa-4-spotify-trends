36ab92a3e7db163a4ae04310aa2f2e88d1943860
JoãoGabriel <jogabriel433@gmail.com>
2025-04-24 15:44:39 -0300

    seeing if actions will turn on and publish it

--- src/routes/global/+page.svelte
+++ src/routes/global/+page.svelte
@@ -1,26 +1,10 @@
 
 <svelte:head>
-  <title>Contact me</title>
+  <title>Global</title>
 </svelte:head>
 
-<form action="mailto:foo@example.com" method="GET">
-    <label for="email">Email: <input id="email" type="email" name="text_email"> </label>
-    <label for="subject">Subject: <input id="subject" type="text" name="text_subject"> </label>
-    <label for="body">Body: <textarea id="body" name="text_message"></textarea> </label>
+<h1>Global</h1>
 
-    <input type="submit" value="Submit" id="button">
-</form>
 <style>
-  form {
-    display: grid;
-    grid-template-columns: auto 1fr; 
-    gap: 1em; 
-  }
-  form label {
-    display: grid;
-    grid-template-columns: subgrid;
-  }
-  #button, label{
-    grid-column: 1 / -1; 
-  }
+
 </style>
