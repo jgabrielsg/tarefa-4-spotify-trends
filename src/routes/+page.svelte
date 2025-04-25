171e788809d8b3f95e8d23371633cd30e6cd3ac6
Vinasque <b51986@fgv.edu.br>
2025-04-25 11:20:52 -0300

    preparei territorio pra começar a mexer com os dados

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script>
-
+    import Chart from "$lib/Chart.svelte";
 </script>
 
 <svelte:head>
   <title>Spotify-Trends</title>
 </svelte:head>
 <h1>Spotify-Trends</h1>
-   
+
+<Chart />   
 
 <style>
 
