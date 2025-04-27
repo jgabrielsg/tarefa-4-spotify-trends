eb47e17b0cfefbf6e890fa7fdb09e5f8b6ac000e
JoãoGabriel <jogabriel433@gmail.com>
2025-04-27 12:28:55 -0300

    exploração em python

---
+++ python/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Para criar o charts_global
+#df = pd.read_csv('charts.csv', low_memory=False)
+#df_global = df[df["region"]=="Global"]
+
+#df_global.to_csv('charts_global.csv', index=False)
+
+df = pd.read_csv('charts.csv', low_memory=False)
