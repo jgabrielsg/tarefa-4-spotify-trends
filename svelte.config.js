f05a5536acd1e47757defd525fc44c750f0f7cf2
JoãoGabriel <jogabriel433@gmail.com>
2025-04-29 07:48:53 -0300

    deploy netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({ fallback: '404.html' }),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
-		},
-	},
+  kit: {
+    adapter: adapter({ fallback: '404.html' }),
+    prerender: { default: true },
+    paths: { base: '' }
+  }
 };
 
 export default config;
