9f65a354203586f6483e077fe9d8196998490a79
Gustyper <gu.bianchi.s@gmail.com>
2025-04-28 23:47:22 -0300

    Arrumando path

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	base: '/FGV-VIS-2025/tarefa-4-spotify-trends/',
 	plugins: [sveltekit()]
 });
